778e0b376cb54e508b0527faaf1010f754d0eabc
solzboy <solomontuiso@gmail.com>
2020-08-23 23:24:39 +0100

    tis

--- resources/views/plans.blade.php
+++ resources/views/plans.blade.php
@@ -29,6 +29,7 @@
 	</div><!-- .container end -->
 </section><!-- #page-title end -->
 
+
 <!-- Pricing Table #1
 ============================================= -->
 <section id="pricing1" class="pricing pricing-1 bg-gray pb-90">
@@ -230,152 +231,22 @@
 					<!--  Pricing heading  -->
 				</div>
 			</div>
-            <!-- .pricing-table End -->
-            
-    <section class="container-fluid bg-info font-white text-white " id="services">
-        <div class="container-fluid">
+            <!-- .pricing-table End -->   
+</section>
 
-                <div class="section-heading `section`-heading--center ">
-
-                    <h2 class="__title font-white">Why choose Axoscrypto Investment plans?</h2>
-                    <p>Exclusive benefits</p>
-                </div>
-
-            <div class="row">
-                <!-- start item -->
-                <div class="col col--sm-6 col--lg-4">
-                    <div class="__item aos-init " data-aos="fade" data-aos-delay="100" data-aos-offset="100">
-                        <div class="__content">
-                            <h6 class="font-white margin_50-bottom __title">We offer great weekly investment profit</h6>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end item -->
-
-                <!-- start item -->
-                <div class="col col--sm-6 col--lg-4">
-                    <div class="__item aos-init" data-aos="fade" data-aos-delay="150" data-aos-offset="100">
-                        <div class="__content">
-                            <h6 class="font-white margin_50-bottom __title">Earn Axos tokens when you purchase a plan membership</h6>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end item -->
-
-                <!-- start item -->
-                <div class="col col--sm-6 col--lg-4">
-                    <div class="__item aos-init " data-aos="fade" data-aos-delay="200" data-aos-offset="100">
-                        <div class="__content">
-                            <h6 class="font-white margin_50-bottom __title">Sell your mining rig anytime</h6>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end item -->
-
-                <!-- start item -->
-                <div class="col col--sm-6 col--lg-4">
-                    <div class="__item aos-init" data-aos="fade" data-aos-delay="250" data-aos-offset="100">
-                        <div class="__content">
-                            <h6 class="font-white margin_50-bottom __title">No limit investing your cryptocurrency</h6>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end item -->
-
-                <!-- start item -->
-                <div class="col col--sm-6 col--lg-4">
-                    <div class="__item aos-init " data-aos="fade" data-aos-delay="300" data-aos-offset="100">
-                        <div class="__content">
-                            <h6 class="font-white margin_50-bottom __title">Earn extra bonus when you use your referral link to refer an investor</h6>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end item -->
-
-                <!-- start item -->
-                <div class="col col--sm-6 col--lg-4">
-                    <div class="__item aos-init " data-aos="fade" data-aos-delay="350" data-aos-offset="100">
-                        <div class="__content">
-                            <h6 class="font-white margin_50-bottom __title">Decrease in cryptocurrency invested do not affect your weekly investment profit</h6>
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end item -->
-            </div>
-
-
+<section class="bg-green">
+    <div class="row">
+        <div>
+        
         </div>
-    </section>
+    </div>
+</section>
 
-			
-			
-<!-- Testimonial #1
-============================================= -->
-<section id="testimonial1" class="testimonial testimonial-1 pt-90 ">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12 col-md-12 col-lg-10 offset-lg-1">
-                <div class="carousel owl-carousel  " data-slide="1" data-slide-rs="1" data-autoplay="true" data-nav="false" data-dots="false" data-space="0" data-loop="true" data-speed="800">
-					<!-- Testimonial #1 -->
-					<div class="testimonial-panel">
-					<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
-						<div class="testimonial--body">
-							<p>“Highly recommended & a great experience. The process was simple and easy to understand. Trading was straight forward, supports all major cryptocurrencies and the entire process was super smooth!”</p>
-						</div><!-- .testimonial-body end -->
-						
-						<div class="testimonial--meta">
-							<div class="testimonial--meta-img">
-								<img src="assets/images/testimonial/2.png" alt="Testimonial Author">
-							</div>
-							<h4>Mahmoud Baghagho</h4>
-							<p>7oroof Agency</p>
-						</div><!-- .testimonial-meta end -->
-					</div><!-- .testimonial-panel end -->
-					
-					<!-- Testimonial #2 -->
-					<div class="testimonial-panel">
-						<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
-						<div class="testimonial--body">
-							<p>“Highly recommended & a great experience. The process was simple and easy to understand. Trading was straight forward, supports all major cryptocurrencies and the entire process was super smooth!”</p>
-						</div><!-- .testimonial-body end -->
-						
-						<div class="testimonial--meta">
-							<div class="testimonial--meta-img">
-								<img src="assets/images/testimonial/1.png" alt="Testimonial Author">
-							</div>
-							<h4>ayman fikry</h4>
-							<p>zytheme</p>
-						</div><!-- .testimonial-meta end -->
-					</div><!-- .testimonial-panel end -->
-					
-					<!-- Testimonial #3 -->
-					<div class="testimonial-panel">
-						<div class="testimonial--icon"></div><!-- .testimonial-icon end -->
-						<div class="testimonial--body">
-							<p>“Highly recommended & a great experience. The process was simple and easy to understand. Trading was straight forward, supports all major cryptocurrencies and the entire process was super smooth!”</p>
-						</div><!-- .testimonial-body end -->
-						
-						<div class="testimonial--meta">
-							<div class="testimonial--meta-img">
-								<img src="assets/images/testimonial/3.png" alt="Testimonial Author">
-							</div>
-							<h4>Fouad badawy</h4>
-							<p>Tie Labs Inc</p>
-						</div><!-- .testimonial-meta end -->
-					</div><!-- .testimonial-panel end -->
-				</div>
-			</div><!-- .col-lg-12 end -->
-		</div><!-- .row end -->
-	</div><!-- .container end -->
-</section><!-- #testimonial1 end -->
 
 <style>
+    .bg-green{
+        background-color:#111D26;
+    }
     .price-bg {
         background-color: #3773F4;
     }
