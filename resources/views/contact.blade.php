304968760906b66002ae69f4eacab8ecfd3e781f
solzboy <solomontuiso@gmail.com>
2020-08-24 22:04:08 +0100

    trading widget

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -93,7 +93,7 @@
     <!-- TradingView Widget BEGIN -->
     <div style="width: 100%; height:auto; background-color: #FFFFFF; overflow:hidden; box-sizing: border-box; border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; block-size:62px; font-size: 12px; box-sizing:content-box; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #141B1A;padding:1px;padding: 0px; margin: 0px;">
         <div style="height:40px;">
-            <iframe src="./AxosCrypto cont_files/widget.html" width="100%" height="36" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe>
+            <iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover=" width="100%" height="36" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;" width="100%" height="36" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe>
         </div>
     </div>
 <!-- TradingView Widget END -->
