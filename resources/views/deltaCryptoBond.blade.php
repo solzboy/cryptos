daf071f61d0ac160b087eb47dd299051df686829
solzboy <solomontuiso@gmail.com>
2020-08-27 20:37:00 +0100

    photos

---
+++ resources/views/deltaCryptoBond.blade.php
@@ -0,0 +1,210 @@
+@extends('layouts/app') @section('title', 'Blue Chips Crypto Bond - ') 
+
+@section('content')
+
+<!-- Page Title #1
+============================================= -->
+<section id="page-title" class="page-title  bg-overlay bg-overlay-dark bg-green bg-parallax">
+<div class="bg-section ">
+        <img src="assets/images/page-titles/3.jpg" alt="Background" />
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <div class="title title-6 text-center">
+                    <div class="title--heading">
+                        <h1>Delta Crypto Bond</h1>
+                    </div>
+                    <div class="clearfix"></div>
+                    <ol class="breadcrumb">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/why-invest">Why Invest</a></li>
+                        <li class="active">Blue Chips Crypto Bond</li>
+                    </ol>
+                </div><!-- .title end -->
+            </div><!-- .col-lg-12 end -->
+        </div><!-- .row end -->
+    </div><!-- .container end -->
+</section><!-- #page-title end -->
+
+
+<!-- Featured #1
+============================================= -->
+<section class="mt-5 mb-5 p-5">
+<div class="container-fluid d-flex justify-content-center ">
+        <div class="row pl-5 ml-5">
+                <div class="col-md-6 col-lg-6 col-sm-12"> 
+                    <div class="container">
+                        <h1 class="hm">Delta Crypto Mining Bond </h1>
+                        <strong> <p class="sg lead text-left"> The Delta Crypto Mining Bond (“DCMB”) is building a bridge from the blockchain and cryptocurrency sector to traditional debt capital markets with the world’s first listed bond to offer investors a passive fixed return, through exposure to cryptocurrency mining. </p></strong> 
+                        <p class="lead text-left"> The DCMB is a bond or debt security issued by Delta Global Securities Ltd.(“DGS”), acting in respect of its segregated account 2 ‘Delta Crypto Mining Bond’ (the “Issuer”), which is listed on the Bermuda Stock Exchange and was created to fund the mining of cryptocurrencies and related projects.</p>
+                    </div>
+                    <div class="row  pl-3">
+                    <div class="col-3  mr-4">
+                         <button type="submit" class="btn  border-dark ">KEY FEATURES</button>
+                    </div>
+                    <div class="col-6">
+                        <button type="submit" class="btn  border-dark ml-3">REGISTER INTEREST</button>
+                    </div>
+                    </div>
+                </div>
+                <div class="container my-auto col-md-6 col-sm-12 ">
+                        <img class="w-75 border-0 d-flex " src="assets/images/icons/DVC-icon-3.png" alt="FirstImg">
+                 </div>
+        </div>
+    </div>
+</section>
+<!-- Featured ENDS
+============================================= -->
+<!-- Featured #2
+============================================= -->
+<section class=" mb-4 p-5 bg-gradient mv ">
+    <div class="justify-content-center p-5">
+        <p class="heading-xs text-white text-center">BLUE CHIPS NETWORK MINING BONG</p>
+    <h1 class="hm text-white text-center">KEY FEATURES</h1>
+                    
+    </div>
+    <div class="row text-left p-5">
+        <div class="col-5 bg-dark">
+                    <!-- start item -->
+                <div class="">
+                    <h6 class=" mb-5 lead text-white"><strong>We offer great weekly investment profit</strong></h6>  
+                </div>
+
+                <!-- start item -->
+                <div class="">
+                    <h6 class="text-white lead mb-5"><strong>Decrease in cryptocurrency invested do not affect your weekly investment profit</strong></h6>
+                </div>
+                <!-- end item -->
+
+                <!-- start item -->
+                <div class="">
+                    <h6 class="text-white lead mb-5"><strong>Decrease in cryptocurrency invested do not affect your weekly investment profit</strong></h6>
+                </div>
+                <!-- end item -->
+        </div>
+
+        <div class="col-5 bg-warning">
+            <div class="container ">
+                <ul>
+                    <li><img class="w-25 clearfix" src="assets/images/icons/Receive-Original-Investment-288x300.png" alt="sduusd" style="font-size:24px; border-width:1px;border-style:none; background:rgba(255,255,255,0.01); color:#333333; border-color:#333333; "> <h6> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aut aperiam itati</h6>  </li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+                
+</section>
+            <!-- TradingView Widget BEGIN -->
+            <div style="width: 100%; height:auto; background-color: #FFFFFF; overflow:hidden; box-sizing: border-box; border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; block-size:62px; font-size: 12px; box-sizing:content-box; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #111D26;padding:1px;padding: 0px; margin: 0px;">
+                    <div style="height:40px;">
+                        <iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=light&pref_coin_id=1505&invert_hover=" width="100%" height="36" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe>
+                    </div>
+                </div>
+            <!-- TradingView Widget END -->
+
+
+
+
+
+
+<!-- Featured ENDS
+============================================= -->
+<!-- Featured #3
+============================================= -->
+
+
+
+
+
+<!-- Featured ENDS
+============================================= -->
+    
+
+
+
+
+
+    <style>
+        .hm{
+        position: relative;
+        letter-spacing: 0.1em;
+        font-size: 1.07em;
+        font-family: Poppins, Arial, sans-serif;  
+        font-weight: 200;
+        line-height: 1.33;
+        color: #28384c;
+        font-size: 2.4em ;
+    }
+    .sg {
+        font-size: 1.47em;
+        line-height: 1.68;
+      
+        color: #28384c;
+
+        font-family: Poppins, Arial, sans-serif; 
+        
+    }
+    body{
+        font-family: Poppins, Arial, sans-serif; 
+        color: #28384c;
+
+    }
+    p{
+        color: #28384c;
+    }
+    .bg-gradient{
+        background-color: #27067B;
+
+        background-image: linear-gradient(to right, #27067B, #1B30A4);
+    }
+    .heading-xs{
+        position: relative;
+        text-transform: uppercase;
+        color:#28384c;
+        letter-spacing: 0.2em;
+        font-weight: 100;
+        font-size: 1.0em ;
+    }
+    .mv ul {
+        list-style: none;
+        font-weight: 300;
+        font-size: 15px;
+        color: #a2b4d2;
+    }
+    .mv ul  li::before {
+        content: "\2022";
+        color: #00CCFF;
+        font-weight: bold;
+        line-height: 2;
+        display: inline-block;
+        width:1em;
+        margin-left: -1em;
+    }
+    
+    .mv{
+        font-family: Poppins, Arial, sans-serif;    
+    }
+    .hm{
+        position: relative;
+        text-transform: uppercase;
+        color: #7ab9ff;
+        letter-spacing: 0.2em;
+        z-index: 2;
+        font-size: 1.07em;
+        font-family: Poppins, Arial, sans-serif;
+        line-height: 1.33;    
+        font-weight: 100;
+    }
+    .team{
+        line-height: 1.86;
+        font-weight: 300;
+        text-align: justify;
+        font-size: 1em;
+        color: #28384c;
+        font-family: "Poppins", sans-serif;
+    }
+    </style>
+
+@endsection
