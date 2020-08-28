f644e94998ac9dc264dee90bafef69a80c7d99ef
David Adokuru <davidadokuru@gmail.com>
2020-08-28 15:08:50 +0100

    updates to keneal

--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,61 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/about-us', function () {
     return view('about');
 });
-
 Route::get('/why-invest', function () {
     return view('whyInvest');
 });
-
 Route::get('/plans', function () {
     return view('plans');
 });
-
 Route::get('/contact-us', function () {
     return view('contact');
 });
 Route::get('/delta-crypto-bond', function () {
     return view('deltaCryptoBond');
 });
-Route::get('/users/plans', function () {
-    return view('users.plans');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['role:User']], function () {
+    Route::get('/basic', function () {
+        return view('welcome');
+    });
+    Route::get('/silver', function () {
+        return view('welcome');
+    });
+    Route::get('/gold', function () {
+        return view('welcome');
+    });
+    Route::get('/platnuim', function () {
+        return view('welcome');
+    });
+    Route::get('/users/plans', function () {
+        return view('users.plans');
+    });
+});
+
+Route::group(['middleware' => ['role:Admin']], function () {
+    Route::get('/basic', function () {
+        return view('welcome');
+    });
+    Route::get('/silver', function () {
+        return view('welcome');
+    });
+    Route::get('/gold', function () {
+        return view('welcome');
+    });
+    Route::get('/platnuim', function () {
+        return view('welcome');
+    });
+    
 });
 
 
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
